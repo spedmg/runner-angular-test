f7d7050437b3d9ad2853b50898f7f34a94cef623
Hector Alvarez <hector.alvarez@strandmesh.com>
2022-07-24 05:03:02 -0700

    feature: generate title type

---
+++ src/app/data/title.ts
@@ -0,0 +1,11 @@
+export interface Title {
+  id: string | number;
+  name: string;
+  level_1_title: string | null;
+  full_name: string;
+  external_id: number;
+  season_number: string | number | null;
+  episode_number: string | number | null;
+  title_level: any;
+}
+
