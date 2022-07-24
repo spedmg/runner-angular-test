a48f8dcd07721f3271aca924d6da34c3a976f880
Hector Alvarez <hector.alvarez@strandmesh.com>
2022-07-24 05:01:18 -0700

    feature: generate typeahead component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { TypeaheadComponent } from './typeahead/typeahead.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    TypeaheadComponent,
   ],
   imports: [
     BrowserModule,
